3cafe284638ba1c567c36318a26e2ed6546f505d
MechOrigin <matt__tatum@msn.com>
2020-03-26 09:08:39 -0700

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -17,7 +17,7 @@ const http = require('http');
 const functions = require('firebase-functions');
 
 const host = 'api.worldweatheronline.com';
-const wwoApiKey = '9648b1724d6c1b3023a40af748a3b846';
+const wwoApiKey = '67ea60f8730e49d2b24193336202003';
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((req, res) => {
   // Get the city and date from the request
